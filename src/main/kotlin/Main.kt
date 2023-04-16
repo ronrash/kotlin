8c024a5271c52ec5a3fa2c42fb00b1d79cc0b730
Rohit Prashar <rohit.prashar@hmcts.net>
2023-04-16 13:17:03 +0100

    begginner series for kotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,6 @@
+ fun main(){
+
+     var name:String="Kate"
+     println("hello kotlin ")
+     println(name)
+ }
---
+++ src/main/kotlin/Variables.kt
@@ -0,0 +1,7 @@
+// val are immutable var are not final
+
+fun main(){
+    val name:String="Kate Winslot"
+   // name="" throw compiler error  whereas we can use
+    println(name)
+}
